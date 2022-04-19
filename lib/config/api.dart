2bb19b98eadf444307d235c4fe29451f7cecfcf8
= <=>
2022-04-19 09:40:15 +0530

    Add config

---
+++ lib/config/api.dart
@@ -0,0 +1,4 @@
+const String _apiBaseUrlKey = "API_BASE_URL";
+const String apiBaseUrl = String.fromEnvironment(_apiBaseUrlKey,
+    defaultValue:
+        "https://my-json-server.typicode.com/tryninjastudy/dummyapi/db");
---
+++ lib/config/app.dart
@@ -0,0 +1,3 @@
+const String _appNameKey = "APP_NAME";
+const String appName =
+    String.fromEnvironment(_appNameKey, defaultValue: "Ninja Study");
